24565cae3108371e9525c45ee77ef54940c97d5e
Aniketrana <ranaannu93@gmail.com>
2019-01-02 10:56:54 +0530

    02-01-2019

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -16,7 +16,7 @@ public partial class _Default : System.Web.UI.Page
     }
     protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
     {
-        string str = "";
+        string str = ""; 
         txtFrom.Text = "0." + txtFrom.Text;
         txtTo.Text = "0." + txtTo.Text;
         if (txtResult.Text == "")
